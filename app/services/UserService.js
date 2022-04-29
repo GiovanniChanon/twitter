e99a1378c5c44e970d8b8ced504c417786b30044
GiovanniChanon <giochanon97@gmail.com>
2022-04-29 14:51:16 -0500

    Agregando requerimiento 2 se servicios

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -4,6 +4,16 @@ class UserService {
     static create(id, username, name){
         return new User(id, username, name, "Sin bio")
     }
+    static getInfo(user){
+        const info = []
+
+        info[0] = user.id
+        info[1] = user.username
+        info[2] = user.name
+        info[3] = user.bio
+
+        return info
+    }
 }
 
 module.exports = UserService
