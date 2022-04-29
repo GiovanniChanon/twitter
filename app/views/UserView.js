f2847115bc71d7dce48387cd63eb69cd66f1012f
GiovanniChanon <giochanon97@gmail.com>
2022-04-29 15:54:06 -0500

    Agregando requerimiento 2 de vistas

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -4,6 +4,8 @@ class UserView{
     static createUser(payload){
         if(payload === null){            
             return {error: "payload no existe"}
+        }else if(typeof payload.id === 'number' ||  typeof payload.username !== 'string' || typeof payload.name === 'string'){
+            return {error: "necesitan tener un valor válido"}
         }
         return UserService.create(payload.id, payload.username, payload.name)
     }
