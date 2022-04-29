0a19d2268eae38406be091e6d32f3b41145909eb
GiovanniChanon <giochanon97@gmail.com>
2022-04-29 13:41:16 -0500

    Agregando requerimiento 3

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -11,4 +11,13 @@ describe("Unit Tests for User class", () => {
         expect(user.dateCreated).not.toBeUndefined()
         expect(user.lastUpdated).not.toBeUndefined()
     });
+
+    test('Add getters', () => {
+        const user = new User(1,"GiovanniChanon","Gio","Bio")
+
+        expect(user.getUsername).toBe("GiovanniChanon")
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
+    })
 })
