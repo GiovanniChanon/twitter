cdbedacda966bfd9456c8ca99bebdf7df6802c3c
GiovanniChanon <giochanon97@gmail.com>
2022-04-29 13:35:29 -0500

    Agregando requerimiento 2

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,13 +2,13 @@ const User = require('../../app/models/User');
 
 describe("Unit Tests for User class", () => {
     test('Create an User Object', () => {
-        const user = new User(1,"GiovanniChanon","Gio","Bio","dateCreated","lastUpdated")
+        const user = new User(1,"GiovanniChanon","Gio","Bio")
 
         expect(user.id).toBe(1)
         expect(user.username).toBe("GiovanniChanon")
         expect(user.name).toBe("Gio")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     });
 })
